11d97f8ba911b3e2d8cdc07d99fca0a93b8163de
JCZou <jiachi.zou@gmail.com>
2021-04-21 21:58:06 +0200

    Update i2c and spi header

--- fmt_fmu/src/hal/i2c/i2c_dev.c
+++ fmt_fmu/src/hal/i2c/i2c_dev.c
@@ -21,9 +21,23 @@
  * Date           Author        Notes
  * 2012-04-25     weety         first version
  * 2014-08-03     bernard       fix some compiling warning
- * 2016-06-06	  zoujiachi		add i2c init func
  */
 
+/******************************************************************************
+ * Copyright 2020-2021 The Firmament Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
 #include "hal/i2c_dev.h"
 #include "hal/i2c-bit-ops.h"
 #include "hal/i2c.h"
--- fmt_fmu/src/hal/spi/spi_dev.c
+++ fmt_fmu/src/hal/spi/spi_dev.c
@@ -21,8 +21,23 @@
  * Date           Author       Notes
  */
 
+/******************************************************************************
+ * Copyright 2020-2021 The Firmament Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
 #include "hal/spi.h"
-#include <rtthread.h>
+#include <firmament.h>
 
 rt_err_t rt_spi_bus_init(struct rt_spi_bus* bus, const char* name)
 {
