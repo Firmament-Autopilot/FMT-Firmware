9977e71ada40330d6ce2c2b64dd39714cd5a70a9
JCZou <jiachi.zou@gmail.com>
2021-08-21 17:38:58 +0200

    Record lost messages for blog

--- src/module/log/blog.c
+++ src/module/log/blog.c
@@ -461,6 +461,10 @@ fmt_err_t blog_push_msg(const uint8_t* payload, uint8_t msg_id, uint16_t len)
     if (buffer_is_full(len + 4)) {
         /* do not let it print too fast */
         TIMETAG_CHECK_EXECUTE(blog_buff_full, 1000, ulog_w(TAG, "buffer is full!"););
+
+        bus_index = get_bus_index(msg_id);
+        blog_handle.monitor[bus_index].lost_msg += 1;
+        
         return FMT_EFULL;
     }
 
