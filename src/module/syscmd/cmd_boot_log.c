a9817aa9a4ecd4afcacb363d87adeb6b989f68b0
JCZou <jiachi.zou@gmail.com>
2021-09-28 22:34:21 +0200

    Add id option for boot_log command

--- src/module/syscmd/cmd_boot_log.c
+++ src/module/syscmd/cmd_boot_log.c
@@ -15,39 +15,61 @@
  *****************************************************************************/
 #include <firmament.h>
 
+#include <dfs_posix.h>
 #include <stdlib.h>
 #include <string.h>
-#include <dfs_posix.h>
 
 #include "module/syscmd/optparse.h"
 #include "module/syscmd/syscmd.h"
 
 #define LOG_SESSION_FILE "/log/session_id"
 
-extern void cat(const char *filename);
+extern void cat(const char* filename);
 
 int cmd_boot_log(int argc, char** argv)
 {
-    int fd = open(LOG_SESSION_FILE, O_WRONLY);
+    struct optparse options;
+    int option;
     char id_buffer[5] = { 0 };
+    int id = -1;
+    struct optparse_long longopts[] = {
+        { "id", 'i', OPTPARSE_REQUIRED },
+        { NULL } /* Don't remove this line */
+    };
 
-    if (fd < 0) {
-        printf("open %s fail!\n", LOG_SESSION_FILE);
-        return EXIT_FAILURE;
+    optparse_init(&options, argv);
+
+    while ((option = optparse_long(&options, longopts, NULL)) != -1) {
+        switch (option) {
+        case 'i':
+            id = atoi(options.optarg);
+            break;
+        case '?':
+            printf("%s: %s\n", "mcn echo", options.errmsg);
+            return EXIT_FAILURE;
+        }
     }
-    /* read session id from file */
-    if (read(fd, id_buffer, sizeof(id_buffer) - 1) > 0) {
+
+    if (id < 0) {
+        /* read current work id */
+        int fd = open(LOG_SESSION_FILE, O_WRONLY);
+        if (fd < 0) {
+            printf("open %s fail!\n", LOG_SESSION_FILE);
+            return EXIT_FAILURE;
+        }
+
+        if (read(fd, id_buffer, sizeof(id_buffer) - 1) > 0) {
+            id = atoi(id_buffer);
+        }
+        close(fd);
+    }
+
+    if (id > 0) {
         char filename[20];
-        int id = atoi(id_buffer);
         console_format(filename, "/log/session_%d/boot_log.txt", id);
         printf("content of %s:\n", filename);
         cat(filename);
-    } else {
-        printf("read %s fail!\n", LOG_SESSION_FILE);
-        close(fd);
-        return EXIT_FAILURE;
     }
-    close(fd);
 
     return EXIT_SUCCESS;
 }
