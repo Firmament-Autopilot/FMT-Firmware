12511d9024030599849e07c7fd2c35dc1bf5e21e
jiachi <jiachi.zou@gmail.com>
2022-01-05 20:20:35 +0100

    Map qemu second serial device

--- target/qemu/qemu-vexpress-a9/qemu.sh
+++ target/qemu/qemu-vexpress-a9/qemu.sh
@@ -3,4 +3,4 @@ qemu-img create -f raw sd.bin 64M
 mkfs.vfat sd.bin
 fi
 
-qemu-system-arm -M vexpress-a9 -kernel build/fmt_fmu.bin -nographic -sd sd.bin
+qemu-system-arm -M vexpress-a9 -kernel build/fmt_fmu.bin -display none -sd sd.bin -serial stdio -serial udp::14550
