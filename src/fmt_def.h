ff6e110d8b10d3b605f7827485f5cdd3de79f6cc
JCZou <jiachi.zou@gmail.com>
2022-05-30 12:03:52 +0800

    FMT-Firmware v0.3.0

--- src/fmt_def.h
+++ src/fmt_def.h
@@ -24,7 +24,7 @@ extern "C" {
 #endif
 
 /* Firmament version information */
-#define FMT_VERSION "v0.3.0-rc"
+#define FMT_VERSION "v0.3.0"
 
 /* Thread Prority */
 #define VEHICLE_THREAD_PRIORITY    3
