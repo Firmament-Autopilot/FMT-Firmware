6f02df6234a740288197e5e89d4023df73796aeb
JCZou <jiachi.zou@gmail.com>
2022-10-31 16:31:56 +0800

    Add MAV_SYS_ID and MAV_COMP_ID parameter

--- src/module/system/sys_param.c
+++ src/module/system/sys_param.c
@@ -20,6 +20,10 @@
 
 /* define parameters */
 static param_t __param_list[] = {
+    /* Mavlink system ID */
+    PARAM_UINT8(MAV_SYS_ID, 1),
+    /* Mavlink component ID */
+    PARAM_UINT8(MAV_COMP_ID, 1),
     /* Determines when to start and stop logging.
 	0: disabled
 	1: when armed until disarm
