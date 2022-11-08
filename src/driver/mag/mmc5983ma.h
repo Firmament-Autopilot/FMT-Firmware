09a74b7a3b61f5292cbdd1d3ff57fc074847e8f7
liuwei <wei.liu@cecooleye.cn>
2022-11-08 05:46:12 -0800

    add mag mmc5983ma

---
+++ src/driver/mag/mmc5983ma.h
@@ -0,0 +1,31 @@
+/******************************************************************************
+ * Copyright 2022 The Firmament Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+#ifndef MMC5983MA_H__
+#define MMC5983MA_H__
+
+#include <rtthread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+rt_err_t drv_mmc5983ma_init(const char* i2c_device_name, const char* mag_device_name);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
