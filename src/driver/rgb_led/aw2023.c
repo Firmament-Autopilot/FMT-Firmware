6d1ddafe462c7b0309c85083fab724bfd60796c6
JCZou <jiachi.zou@gmail.com>
2022-11-27 14:20:14 +0800

    Improve icf5 i2c driver speed

--- src/driver/rgb_led/aw2023.c
+++ src/driver/rgb_led/aw2023.c
@@ -16,7 +16,7 @@
 #include "driver/rgb_led/aw2023.h"
 #include "hal/i2c/i2c.h"
 
-#define DRV_DBG(...) console_printf(__VA_ARGS__)
+#define DRV_DBG(...) printf(__VA_ARGS__)
 // #define DRV_DBG(...)
 
 #define AW2023_REG_RSTR   0x00
@@ -41,119 +41,148 @@
 #define AW2023_REG_LED2T1 0x3E
 #define AW2023_REG_LED2T2 0x3F
 
-#define AW2023_CHIP_ID 0x09
+#define AW2023_CHIP_ID            0x09
+#define AW2023_CHIP_RESER         0x55
+#define AW2023_CHIP_EN            BIT(0)
+#define AW2023_IMAX_15mA          0
+#define AW2023_IMAX_30mA          BIT(0)
+#define AW2023_IMAX_5mA           BIT(1)
+#define AW2023_IMAX_10mA          BIT(0) | BIT(1)
+#define AW2023_FREQ_250Hz         0
+#define AW2023_FREQ_125Hz         BIT(5)
+#define AW2023_PWM_LIN            BIT(3)
+#define AW2023_LED2_EN            BIT(2)
+#define AW2023_LED1_EN            BIT(1)
+#define AW2023_LED0_EN            BIT(0)
+#define AW2023_LED_SYNC_MODE      BIT(7)
+#define AW2023_LED_FADE_OUT       BIT(6)
+#define AW2023_LED_FADE_IN        BIT(5)
+#define AW2023_LED_PATERN_MODE    BIT(4)
+#define AW2023_LED_CURRENT(cur)   (cur & 0x0F)
+#define AW2023_PWM_DIMMING(dim)   (dim & 0xFF)
+#define AW2023_T1_0_0s            0
+#define AW2023_T1_0_13s           BIT(4)
+#define AW2023_T1_0_26s           BIT(5)
+#define AW2023_T1_0_38s           BIT(4) | BIT(5)
+#define AW2023_T1_0_51s           BIT(6)
+#define AW2023_T1_0_77s           BIT(4) | BIT(6)
+#define AW2023_T1_1_04s           BIT(5) | BIT(6)
+#define AW2023_T1_1_6s            BIT(4) | BIT(5) | BIT(6)
+#define AW2023_T1_2_1s            BIT(7)
+#define AW2023_T1_2_6s            BIT(4) | BIT(7)
+#define AW2023_T1_3_1s            BIT(5) | BIT(7)
+#define AW2023_T1_4_2s            BIT(4) | BIT(5) | BIT(7)
+#define AW2023_T1_5_2s            BIT(6) | BIT(7)
+#define AW2023_T1_6_2s            BIT(4) | BIT(6) | BIT(7)
+#define AW2023_T1_7_3s            BIT(5) | BIT(6) | BIT(7)
+#define AW2023_T1_8_3s            BIT(4) | BIT(5) | BIT(6) | BIT(7)
+#define AW2023_T2_0_04s           0
+#define AW2023_T2_0_13s           BIT(0)
+#define AW2023_T2_0_26s           BIT(1)
+#define AW2023_T2_0_38s           BIT(0) | BIT(1)
+#define AW2023_T2_0_51s           BIT(2)
+#define AW2023_T2_0_77s           BIT(0) | BIT(2)
+#define AW2023_T2_1_04s           BIT(1) | BIT(2)
+#define AW2023_T2_1_6s            BIT(0) | BIT(1) | BIT(2)
+#define AW2023_T2_2_1s            BIT(3)
+#define AW2023_T2_2_6s            BIT(0) | BIT(3)
+#define AW2023_T2_3_1s            BIT(1) | BIT(3)
+#define AW2023_T2_4_2s            BIT(0) | BIT(1) | BIT(3)
+#define AW2023_T2_5_2s            BIT(2) | BIT(3)
+#define AW2023_T2_6_2s            BIT(0) | BIT(2) | BIT(3)
+#define AW2023_T2_7_3s            BIT(1) | BIT(2) | BIT(3)
+#define AW2023_T2_8_3s            BIT(0) | BIT(1) | BIT(2) | BIT(3)
+#define AW2023_T3_0_0s            0
+#define AW2023_T3_0_13s           BIT(4)
+#define AW2023_T3_0_26s           BIT(5)
+#define AW2023_T3_0_38s           BIT(4) | BIT(5)
+#define AW2023_T3_0_51s           BIT(6)
+#define AW2023_T3_0_77s           BIT(4) | BIT(6)
+#define AW2023_T3_1_04s           BIT(5) | BIT(6)
+#define AW2023_T3_1_6s            BIT(4) | BIT(5) | BIT(6)
+#define AW2023_T3_2_1s            BIT(7)
+#define AW2023_T3_2_6s            BIT(4) | BIT(7)
+#define AW2023_T3_3_1s            BIT(5) | BIT(7)
+#define AW2023_T3_4_2s            BIT(4) | BIT(5) | BIT(7)
+#define AW2023_T3_5_2s            BIT(6) | BIT(7)
+#define AW2023_T3_6_2s            BIT(4) | BIT(6) | BIT(7)
+#define AW2023_T3_7_3s            BIT(5) | BIT(6) | BIT(7)
+#define AW2023_T3_8_3s            BIT(4) | BIT(5) | BIT(6) | BIT(7)
+#define AW2023_T4_0_04s           0
+#define AW2023_T4_0_13s           BIT(0)
+#define AW2023_T4_0_26s           BIT(1)
+#define AW2023_T4_0_38s           BIT(0) | BIT(1)
+#define AW2023_T4_0_51s           BIT(2)
+#define AW2023_T4_0_77s           BIT(0) | BIT(2)
+#define AW2023_T4_1_04s           BIT(1) | BIT(2)
+#define AW2023_T4_1_6s            BIT(0) | BIT(1) | BIT(2)
+#define AW2023_T4_2_1s            BIT(3)
+#define AW2023_T4_2_6s            BIT(0) | BIT(3)
+#define AW2023_T4_3_1s            BIT(1) | BIT(3)
+#define AW2023_T4_4_2s            BIT(0) | BIT(1) | BIT(3)
+#define AW2023_T4_5_2s            BIT(2) | BIT(3)
+#define AW2023_T4_6_2s            BIT(0) | BIT(2) | BIT(3)
+#define AW2023_T4_7_3s            BIT(1) | BIT(2) | BIT(3)
+#define AW2023_T4_8_3s            BIT(0) | BIT(1) | BIT(2) | BIT(3)
+#define AW2023_T0_0_4s            0
+#define AW2023_T0_0_13s           BIT(4)
+#define AW2023_T0_0_26s           BIT(5)
+#define AW2023_T0_0_38s           BIT(4) | BIT(5)
+#define AW2023_T0_0_51s           BIT(6)
+#define AW2023_T0_0_77s           BIT(4) | BIT(6)
+#define AW2023_T0_1_04s           BIT(5) | BIT(6)
+#define AW2023_T0_1_6s            BIT(4) | BIT(5) | BIT(6)
+#define AW2023_T0_2_1s            BIT(7)
+#define AW2023_T0_2_6s            BIT(4) | BIT(7)
+#define AW2023_T0_3_1s            BIT(5) | BIT(7)
+#define AW2023_T0_4_2s            BIT(4) | BIT(5) | BIT(7)
+#define AW2023_T0_5_2s            BIT(6) | BIT(7)
+#define AW2023_T0_6_2s            BIT(4) | BIT(6) | BIT(7)
+#define AW2023_T0_7_3s            BIT(5) | BIT(6) | BIT(7)
+#define AW2023_T0_8_3s            BIT(4) | BIT(5) | BIT(6) | BIT(7)
+#define AW2023_REPEAT_TIMES(time) (time & 0x0F)
+
+#ifndef BIT
+    #define BIT(_idx) (1 << _idx)
+#endif
+#define REG_VAL(_setbits, _clearbits) \
+    (reg_val_t) { .setbits = (_setbits), .clearbits = (_clearbits) }
+
+typedef struct {
+    uint8_t setbits;
+    uint8_t clearbits;
+} reg_val_t;
 
 static rt_device_t i2c_dev;
 
-static void send_led_rgb(void)
+static rt_err_t i2c_write_checked_reg(rt_device_t i2c_dev, uint8_t reg, uint8_t val)
 {
-    /* Do not pack 3 data into a single msg, otherwise color set would fail some how,
-     * it could because that the ncp5623 speed is relatively slow */
-    // struct rt_i2c_msg msgs[3];
-    // uint8_t data[3] = { NCP5623_LED_PWM0 | _r, NCP5623_LED_PWM1 | _g, NCP5623_LED_PWM2 | _b };
+    uint8_t reg_val;
 
-    // msgs[0].flags = RT_I2C_WR | i2c_device->flags;
-    // msgs[0].buf = &data[0];
-    // msgs[0].len = 1;
+    RT_TRY(i2c_write_reg(i2c_dev, reg, val));
+    RT_TRY(i2c_read_reg(i2c_dev, reg, &reg_val));
 
-    // msgs[1].flags = RT_I2C_WR | i2c_device->flags;
-    // msgs[1].buf = &data[1];
-    // msgs[1].len = 1;
-
-    // msgs[2].flags = RT_I2C_WR | i2c_device->flags;
-    // msgs[2].buf = &data[2];
-    // msgs[2].len = 1;
-
-    // rt_size_t ret = rt_i2c_transfer(i2c_device->bus, i2c_device->slave_addr, msgs, 3);
-    // if (ret != 3) {
-    //     DRV_DBG("set led rgb fail!\n");
-    // }
-}
-
-static void send_led_bright(void)
-{
-    // struct rt_i2c_msg msgs;
-    // uint8_t data = { NCP5623_LED_CURRENT | _brightness };
-
-    // msgs.flags = RT_I2C_WR | i2c_device->flags;
-    // msgs.buf = &data;
-    // msgs.len = 1;
-
-    // rt_size_t ret = rt_i2c_transfer(i2c_device->bus, i2c_device->slave_addr, &msgs, 1);
-    // if (ret != 1) {
-    //     PERIOD_EXECUTE(dbg, 1000, DRV_DBG("set led bright fail! IC1 ISR:0x%x ret:%d\n", I2C1->ISR, ret););
-    // }
-}
-
-static rt_err_t led_shutdown(void)
-{
-    // struct rt_i2c_msg msgs;
-    // uint8_t data = NCP5623_LED_OFF;
-
-    // msgs.flags = RT_I2C_WR | i2c_device->flags;
-    // msgs.buf = &data;
-    // msgs.len = 1;
-
-    // if (rt_i2c_transfer(i2c_device->bus, i2c_device->slave_addr, &msgs, 1) != 1) {
-    //     DRV_DBG("rgb led shutdown fail\n");
-    //     return RT_ERROR;
-    // }
+    if (reg_val != val) {
+        DRV_DBG("checked reg %x fail:%x %x\n", reg, val, reg_val);
+        return RT_ERROR;
+    }
 
     return RT_EOK;
 }
 
-static rt_err_t aw2023_control(rt_device_t dev, int cmd, void* args)
+static rt_err_t i2c_modify_reg(rt_device_t i2c_dev, uint8_t reg, reg_val_t reg_val)
 {
-    // uint8_t color;
+    uint8_t value;
 
-    // switch (cmd) {
-    // case NCP5623_CMD_SET_COLOR:
-    //     color = (uint32_t)args;
-    //     if (color == NCP5623_LED_RED) {
-    //         _r = LED_BRIGHT;
-    //         _g = 0;
-    //         _b = 0;
-    //     } else if (color == NCP5623_LED_GREEN) {
-    //         _r = 0;
-    //         _g = LED_BRIGHT;
-    //         _b = 0;
-    //     } else if (color == NCP5623_LED_BLUE) {
-    //         _r = 0;
-    //         _g = 0;
-    //         _b = LED_BRIGHT;
-    //     } else if (color == NCP5623_LED_YELLOW) {
-    //         _r = LED_BRIGHT;
-    //         _g = LED_BRIGHT;
-    //         _b = 0;
-    //     } else if (color == NCP5623_LED_PURPLE) {
-    //         _r = LED_BRIGHT;
-    //         _g = 0;
-    //         _b = LED_BRIGHT;
-    //     } else if (color == NCP5623_LED_CYAN) {
-    //         _r = 0;
-    //         _g = LED_BRIGHT;
-    //         _b = LED_BRIGHT;
-    //     } else if (color == NCP5623_LED_WHITE) {
-    //         _r = LED_BRIGHT;
-    //         _g = LED_BRIGHT;
-    //         _b = LED_BRIGHT;
-    //     } else {
-    //         _r = 0;
-    //         _g = 0;
-    //         _b = 0;
-    //     }
-    //     send_led_rgb();
-    //     break;
+    /* In case of read operations of the accelerometer part, the requested data is not sent 
+    immediately, but instead first a dummy byte is sent, and after this dummy byte the actual 
+    reqested register content is transmitted. */
+    RT_TRY(i2c_read_reg(i2c_dev, reg, &value));
 
-    // case NCP5623_CMD_SET_BRIGHT:
-    //     _brightness = (uint32_t)args & 0x1F;
-    //     // NOTE: NCP5623C speed is low, so don't write i2c command too fast
-    //     send_led_bright();
-    //     break;
+    value &= ~reg_val.clearbits;
+    value |= reg_val.setbits;
 
-    // default:
-    //     break;
-    // }
+    RT_TRY(i2c_write_checked_reg(i2c_dev, reg, value));
 
     return RT_EOK;
 }
@@ -182,60 +211,48 @@ rt_err_t drv_aw2023_init(const char* i2c_dev_name)
     RT_TRY(probe());
 
     /* reset the chip */
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_RSTR, 0x55));
+    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_RSTR, AW2023_CHIP_RESER));
     systime_mdelay(5);
 
     /* let device enters active state */
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_GCR1, 0x01));
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_GCR1, AW2023_CHIP_EN));
     systime_mdelay(5);
 
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LCTR, 0x00));
+    uint8_t reg_val;
+    RT_TRY(i2c_read_reg(i2c_dev, AW2023_REG_PATST, &reg_val));
+    printf("PATST1:%x\n", reg_val);
 
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LCFG0, 0x03));
-    systime_mdelay(5);
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LCFG1, 0x03));
-    systime_mdelay(5);
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LCFG2, 0x03));
-    systime_mdelay(5);
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LCTR, 0x00));
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LCFG0, 0x00));
 
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LED0T0, 0x77));
-    systime_mdelay(5);
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LED1T0, 0x77));
-    systime_mdelay(5);
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LED2T0, 0x77));
-    systime_mdelay(5);
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_GCR2, AW2023_IMAX_15mA));
+    
 
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LED0T1, 0x77));
-    systime_mdelay(5);
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LED1T1, 0x77));
-    systime_mdelay(5);
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LED2T1, 0x77));
-    systime_mdelay(5);
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LCFG0, AW2023_LED_SYNC_MODE | AW2023_LED_CURRENT(15)));
 
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LED0T2, 0x70));
-    systime_mdelay(5);
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LED1T2, 0x70));
-    systime_mdelay(5);
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LED2T2, 0x70));
-    systime_mdelay(5);
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_PWM0, AW2023_PWM_DIMMING(255)));
 
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_PWM0, 127));
-    systime_mdelay(5);
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_PWM1, 127));
-    systime_mdelay(5);
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_PWM2, 127));
-    systime_mdelay(5);
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LED0T0, AW2023_T1_2_1s | AW2023_T2_2_1s));
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LED0T1, AW2023_T3_2_1s | AW2023_T4_2_1s));
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LED0T2, AW2023_T0_2_1s | AW2023_REPEAT_TIMES(0)));
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LED1T0, AW2023_T1_2_1s | AW2023_T2_2_1s));
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LED1T1, AW2023_T3_2_1s | AW2023_T4_2_1s));
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LED1T2, AW2023_T0_2_1s | AW2023_REPEAT_TIMES(0)));
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LED2T0, AW2023_T1_2_1s | AW2023_T2_2_1s));
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LED2T1, AW2023_T3_2_1s | AW2023_T4_2_1s));
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LED2T2, AW2023_T0_2_1s | AW2023_REPEAT_TIMES(0)));
 
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LCFG0, 0x13));
-    systime_mdelay(5);
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LCFG1, 0x13));
-    systime_mdelay(5);
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LCFG2, 0x13));
-    systime_mdelay(5);
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LCFG0, AW2023_LED_CURRENT(15) | AW2023_LED_PATERN_MODE));
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LCFG1, AW2023_LED_CURRENT(7) | AW2023_LED_PATERN_MODE));
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LCFG2, AW2023_LED_CURRENT(0) | AW2023_LED_PATERN_MODE));
 
-    RT_TRY(i2c_write_reg(i2c_dev, AW2023_REG_LCTR, 0x07));
+    RT_TRY(i2c_write_checked_reg(i2c_dev, AW2023_REG_LCTR, AW2023_FREQ_250Hz | AW2023_LED0_EN | AW2023_LED1_EN | AW2023_LED2_EN));
 
-    // RT_CHECK(rt_device_register(device, "ncp5623c", RT_DEVICE_OFLAG_RDWR));
+    systime_mdelay(10);
+
+    // uint8_t reg_val;
+    RT_TRY(i2c_read_reg(i2c_dev, AW2023_REG_PATST, &reg_val));
+    printf("PATST2:%x\n", reg_val);
 
     return RT_EOK;
 }
