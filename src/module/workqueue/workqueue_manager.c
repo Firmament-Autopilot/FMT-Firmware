80e2e24bfa43edad650586beed34dceeea3684b8
Josh Zou <zoujiachi666@163.com>
2023-02-14 14:28:03 +0800

    Decrease some task's stack size

--- src/module/workqueue/workqueue_manager.c
+++ src/module/workqueue/workqueue_manager.c
@@ -37,10 +37,10 @@ WorkQueue_t workqueue_find(const char* name)
 
 fmt_err_t workqueue_manager_init(void)
 {
-    wq_list[0] = workqueue_create("wq:lp_work", 20, 10240, 19);
+    wq_list[0] = workqueue_create("wq:lp_work", 20, 5 * 1024, 19);
     RT_ASSERT(wq_list[0] != NULL);
 
-    wq_list[1] = workqueue_create("wq:hp_work", 20, 10240, 6);
+    wq_list[1] = workqueue_create("wq:hp_work", 20, 5 * 1024, 6);
     RT_ASSERT(wq_list[1] != NULL);
 
     return FMT_EOK;
--- src/task/vehicle/multicopter/task_vehicle.c
+++ src/task/vehicle/multicopter/task_vehicle.c
@@ -125,7 +125,7 @@ TASK_EXPORT __fmt_task_desc = {
     .entry = task_vehicle_entry,
     .priority = VEHICLE_THREAD_PRIORITY,
     .auto_start = true,
-    .stack_size = 10240,
+    .stack_size = 5 * 1024,
     .param = NULL,
     .dependency = NULL
 };
