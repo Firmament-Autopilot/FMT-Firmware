beabe5b6d3a7674d8444bbf4def5db292c18d225
Josh Zou <zoujiachi666@163.com>
2023-05-15 14:01:04 +0800

    Version 0.5.1

--- src/fmt_def.h
+++ src/fmt_def.h
@@ -24,7 +24,7 @@ extern "C" {
 #endif
 
 /* Firmament version information */
-#define FMT_VERSION "v0.5.0"
+#define FMT_VERSION "v0.5.1"
 
 /* Thread Prority */
 #define VEHICLE_THREAD_PRIORITY    3
