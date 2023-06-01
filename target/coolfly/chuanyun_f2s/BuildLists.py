338d884e5dcb41a7e625d82f7cc510115dddf009
Josh Zou <zoujiachi666@163.com>
2023-06-01 13:40:30 +0800

    Add protocol config file

--- target/coolfly/chuanyun_f2s/BuildLists.py
+++ target/coolfly/chuanyun_f2s/BuildLists.py
@@ -3,6 +3,7 @@
 from config.driver import *
 from config.hal import *
 from config.module import *
+from config.protocol import *
 from config.library import *
 from config.model import *
 from config.task import *
