f4276f765bea8d51258a13e488f14a4a4318854b
Josh Zou <zoujiachi666@163.com>
2023-06-05 21:35:32 +0800

    Remove qemu rx udp port in order to support multi-aircraft

--- target/qemu/qemu-vexpress-a9/qemu.bat
+++ target/qemu/qemu-vexpress-a9/qemu.bat
@@ -2,4 +2,4 @@ if exist sd.bin goto run
 qemu-img create -f raw sd.bin 64M
 
 :run
-qemu-system-arm -M vexpress-a9 -kernel build/fmt_qemu-vexpress-a9.bin -display none -device sd-card,drive=sdcard_drive -drive file=sd.bin,format=raw,id=sdcard_drive -serial stdio -serial udp:127.0.0.1:14550@127.0.0.1:14551 -serial udp:127.0.0.1:14552@127.0.0.1:14553
+qemu-system-arm -M vexpress-a9 -kernel build/fmt_qemu-vexpress-a9.bin -display none -device sd-card,drive=sdcard_drive -drive file=sd.bin,format=raw,id=sdcard_drive -serial stdio -serial udp:127.0.0.1:14550 -serial udp:127.0.0.1:14551
