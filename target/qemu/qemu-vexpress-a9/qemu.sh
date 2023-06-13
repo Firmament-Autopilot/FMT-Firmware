4aef4191cd9e5e1775d3ac20306fbaca62012a19
josh <josh@Ubuntu1804.myguest.virtualbox.org>
2023-06-13 13:16:34 +0800

    Fix qemu.sh error

--- target/qemu/qemu-vexpress-a9/qemu.sh
+++ target/qemu/qemu-vexpress-a9/qemu.sh
@@ -3,4 +3,4 @@ qemu-img create -f raw sd.bin 64M
 mkfs.vfat sd.bin
 fi
 
-qemu-system-arm -M vexpress-a9 -kernel build/fmt_qemu-vexpress-a9.bin -display none -device sd-card,drive=sdcard_drive -drive file=sd.bin,format=raw,id=sdcard_drive -serial stdio -serial udp:localhost:14550 -serial udp:localhost:14551
+qemu-system-arm -M vexpress-a9 -kernel build/fmt_qemu-vexpress-a9.bin -display none -sd sd.bin -serial stdio -serial udp:localhost:14550 -serial udp:localhost:14551
