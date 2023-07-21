fa084bdccb59f738cb773b5be97588b6525508da
Josh Zou <zoujiachi666@163.com>
2023-07-21 17:00:40 +0800

    rename mav_ext_state to external_state

--- src/model/ins/external_ins/ins_interface.c
+++ src/model/ins/external_ins/ins_interface.c
@@ -25,7 +25,7 @@
 
 #define BIT(u, n) (u & (1 << n))
 
-MCN_DECLARE(mav_ext_state);
+MCN_DECLARE(external_state);
 static McnNode_t ext_state_node;
 
 /* INS output bus */
@@ -106,48 +106,48 @@ static int ins_output_echo(void* param)
 void ins_interface_step(void)
 {
     if (mcn_poll(ext_state_node)) {
-        mavlink_fmt_external_state_t mav_ext_state;
+        mavlink_fmt_external_state_t external_state;
         Euler euler;
         INS_Out_Bus ins_out = { 0 };
 
-        mcn_copy(MCN_HUB(mav_ext_state), ext_state_node, &mav_ext_state);
+        mcn_copy(MCN_HUB(external_state), ext_state_node, &external_state);
 
-        ins_out.timestamp = mav_ext_state.timestamp;
+        ins_out.timestamp = external_state.timestamp;
 
-        ins_out.quat[0] = mav_ext_state.quat[0];
-        ins_out.quat[1] = mav_ext_state.quat[1];
-        ins_out.quat[2] = mav_ext_state.quat[2];
-        ins_out.quat[3] = mav_ext_state.quat[3];
+        ins_out.quat[0] = external_state.quat[0];
+        ins_out.quat[1] = external_state.quat[1];
+        ins_out.quat[2] = external_state.quat[2];
+        ins_out.quat[3] = external_state.quat[3];
 
         quaternion_toEuler((quaternion*)ins_out.quat, &euler);
         ins_out.phi = euler.roll;
         ins_out.theta = euler.pitch;
         ins_out.psi = euler.yaw;
 
-        ins_out.p = mav_ext_state.rot[0];
-        ins_out.q = mav_ext_state.rot[1];
-        ins_out.r = mav_ext_state.rot[2];
+        ins_out.p = external_state.rot[0];
+        ins_out.q = external_state.rot[1];
+        ins_out.r = external_state.rot[2];
 
         float acc_B[3];
-        quaternion_inv_rotateVector((quaternion*)ins_out.quat, mav_ext_state.acc, acc_B);
+        quaternion_inv_rotateVector((quaternion*)ins_out.quat, external_state.acc, acc_B);
         ins_out.ax = acc_B[0];
         ins_out.ay = acc_B[1];
         ins_out.az = acc_B[2];
 
-        ins_out.vn = mav_ext_state.vel[0];
-        ins_out.ve = mav_ext_state.vel[1];
-        ins_out.vd = mav_ext_state.vel[2];
+        ins_out.vn = external_state.vel[0];
+        ins_out.ve = external_state.vel[1];
+        ins_out.vd = external_state.vel[2];
 
-        ins_out.lat = DEG2RAD((double)mav_ext_state.lla[0]) * 1e-7;
-        ins_out.lon = DEG2RAD((double)mav_ext_state.lla[1]) * 1e-7;
-        ins_out.alt = (double)mav_ext_state.lla[2] * 1e-3;
+        ins_out.lat = DEG2RAD((double)external_state.lla[0]) * 1e-7;
+        ins_out.lon = DEG2RAD((double)external_state.lla[1]) * 1e-7;
+        ins_out.alt = (double)external_state.lla[2] * 1e-3;
 
-        ins_out.x_R = mav_ext_state.pos[0];
-        ins_out.y_R = mav_ext_state.pos[1];
-        ins_out.h_R = -mav_ext_state.pos[2];
-        ins_out.h_AGL = mav_ext_state.h_AGL;
+        ins_out.x_R = external_state.pos[0];
+        ins_out.y_R = external_state.pos[1];
+        ins_out.h_R = -external_state.pos[2];
+        ins_out.h_AGL = external_state.h_AGL;
 
-        ins_out.flag = mav_ext_state.flag | 1;
+        ins_out.flag = external_state.flag | 1;
 
         /* publish INS output */
         mcn_publish(MCN_HUB(ins_output), &ins_out);
@@ -170,9 +170,9 @@ void ins_interface_init(void)
     /* advertise ins_output topic */
     mcn_advertise(MCN_HUB(ins_output), ins_output_echo);
 
-    ext_state_node = mcn_subscribe(MCN_HUB(mav_ext_state), NULL, NULL);
+    ext_state_node = mcn_subscribe(MCN_HUB(external_state), NULL, NULL);
     if (ext_state_node == NULL) {
-        printf("External_INS fail to subscribe mav_ext_state topic!\n");
+        printf("External_INS fail to subscribe external_state topic!\n");
     }
 
     INS_Out_ID = mlog_get_bus_id("INS_Out");
