67436af982f501e8cfca4b7fc6d32f0228c514da
Josh Zou <zoujiachi666@163.com>
2023-10-26 09:25:09 +0800

    Decrease MAVLINK_MSG_ID_HIL_STATE send freq

--- src/task/comm/mavgcs.c
+++ src/task/comm/mavgcs.c
@@ -457,7 +457,7 @@ fmt_err_t mavgcs_init(void)
     FMT_TRY(mavproxy_register_period_msg(MAVPROXY_GCS_CHAN, MAVLINK_MSG_ID_RC_CHANNELS, 10, mavlink_msg_rc_channels_pack_func, true));
 
 #ifdef FMT_USING_SIH
-    FMT_TRY(mavproxy_register_period_msg(MAVPROXY_GCS_CHAN, MAVLINK_MSG_ID_HIL_STATE, 100, mavlink_msg_hil_state_pack_func, true));
+    FMT_TRY(mavproxy_register_period_msg(MAVPROXY_GCS_CHAN, MAVLINK_MSG_ID_HIL_STATE, 60, mavlink_msg_hil_state_pack_func, true));
 #endif
 
     /* register gcs mavlink handler */
