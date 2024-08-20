9b79c2e5389bf2a5c9070039270d15a3bb7d37d1
Josh Zou <zoujiachi666@163.com>
2024-08-20 13:18:47 +0800

    Enable more modules

--- target/cuav/x7_plus/board/board.c
+++ target/cuav/x7_plus/board/board.c
@@ -456,6 +456,12 @@ void bsp_post_initialize(void)
     /* init gcs */
     FMT_CHECK(gcs_cmd_init());
 
+    /* init auto command */
+    FMT_CHECK(auto_cmd_init());
+
+    /* init mission data */
+    FMT_CHECK(mission_data_init());
+
     /* start device message queue work */
     FMT_CHECK(devmq_start_work());
 
