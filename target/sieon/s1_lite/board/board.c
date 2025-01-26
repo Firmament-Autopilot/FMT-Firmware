8b8ecb9f54ef5d503b342556237c159200233928
Josh Zou <zoujiachi666@163.com>
2025-01-26 16:25:51 +0800

    Add a simple MPU config but not open by default

--- target/sieon/s1_lite/board/board.c
+++ target/sieon/s1_lite/board/board.c
@@ -195,6 +195,31 @@ static fmt_err_t bsp_parse_toml_sysconfig(toml_table_t* root_tab)
     return err;
 }
 
+static void MPU_Config(void)
+{
+    MPU_Region_InitTypeDef MPU_InitStruct = { 0 };
+
+    /* Disable the MPU before configure */
+    HAL_MPU_Disable();
+
+    /* Configure the MPU attributes as WT for AXI SRAM */
+    MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+    MPU_InitStruct.BaseAddress = 0x24000000;
+    MPU_InitStruct.Size = MPU_REGION_SIZE_512KB;
+    MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+    MPU_InitStruct.IsBufferable = MPU_ACCESS_BUFFERABLE;
+    MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+    MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+    MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+    MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+    MPU_InitStruct.SubRegionDisable = 0x00;
+    MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+    HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+    /* Enable the MPU */
+    HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+}
+
 /**
  * @brief  CPU Config.
  * @param  None
@@ -202,8 +227,7 @@ static fmt_err_t bsp_parse_toml_sysconfig(toml_table_t* root_tab)
  */
 static void CPU_Config(void)
 {
-    // __set_PRIMASK(0);
-    // __set_BASEPRI(0);
+    // MPU_Config();
 
     /*
      * When enabling the D-cache there is cache coherency issue.
