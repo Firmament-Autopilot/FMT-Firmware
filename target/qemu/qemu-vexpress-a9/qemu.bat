95511ac53b645f485b9162646e2841d0e77c38c8
Josh Zou <zoujiachi666@163.com>
2025-02-19 17:52:39 +0800

    Modify qemu.bat do not let system lower the qemu task priority

--- target/qemu/qemu-vexpress-a9/qemu.bat
+++ target/qemu/qemu-vexpress-a9/qemu.bat
@@ -3,3 +3,4 @@ qemu-img create -f raw sd.bin 64M
 
 :run
 qemu-system-arm -M vexpress-a9 -kernel build/fmt_qemu-vexpress-a9.bin -display none -device sd-card,drive=sdcard_drive -drive file=sd.bin,format=raw,id=sdcard_drive -serial stdio -serial udp:127.0.0.1:14550 -serial udp:127.0.0.1:14551
+wmic process where name="qemu-system-x86_64.exe" CALL setpriority 128
