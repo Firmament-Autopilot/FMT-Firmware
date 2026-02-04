c2dc2caeb6bd593ba9a69c0c592fa7571461a72b
Rbb666 <zhangbingru@rt-thread.com>
2026-02-04 11:57:56 +0800

    [target/e83]优化M33&55编译方式

--- target/infineon/edge-e83/bootloader/mklinks.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-@echo on
-mklink /D libraries ..\libraries
-mklink /D libs ..\libs
-mklink /D tools ..\tools
--- target/infineon/edge-e83/m33/mklinks.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-@echo on
-mklink /D rt-thread ..\..\..\..\rtos\rt-thread
-mklink /D libraries ..\libraries
-mklink /D libs ..\libs
-mklink /D tools ..\tools
