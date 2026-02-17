e98ce639250f2d587468c270f4bf069f699f194a
Chenliang Pan <panchenliang@nuaa.edu.cn>
2026-02-17 22:47:37 +0800

    fix i2c bug

--- target/pixhawk/fmu-v6c/drivers/drv_i2c.h
+++ target/pixhawk/fmu-v6c/drivers/drv_i2c.h
@@ -26,12 +26,7 @@ extern "C" {
 #define NCP5623C_ADDRESS 0x39
 #define MS5611_ADDRESS   0x77
 
-/* Note: currently using software I2C as a temporary fallback while
-* hardware I2C is being diagnosed. Replace `drv_i2c_soft_init()` with
-* the hardware I2C init when available. */
-
 rt_err_t drv_i2c_init(void);
-//rt_err_t drv_i2c_soft_init(void);
 
 #ifdef __cplusplus
 }
