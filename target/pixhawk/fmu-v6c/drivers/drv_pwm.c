965acdd6de9c90e4681af3f13008af7a0f3ca03b
Chenliang Pan <panchenliang@nuaa.edu.cn>
2026-02-17 22:47:37 +0800

    fix i2c bug

--- target/pixhawk/fmu-v6c/drivers/drv_pwm.c
+++ target/pixhawk/fmu-v6c/drivers/drv_pwm.c
@@ -63,9 +63,9 @@ void pwm_gpio_init(void)
     LL_AHB1_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOD);
     /**TIM4 GPIO Configuration
     PD14     ------> TIM4_CH3
-    PD13     ------> TIM4_CH2
+    PD15     ------> TIM4_CH4
     */
-    GPIO_InitStruct.Pin = LL_GPIO_PIN_13 | LL_GPIO_PIN_14;
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_14 | LL_GPIO_PIN_15;
     GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
     GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
