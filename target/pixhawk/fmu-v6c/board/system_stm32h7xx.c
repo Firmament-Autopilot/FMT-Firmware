33e5b6011b4602f98c446449e877927b4010c65f
Chenliang Pan <panchenliang@nuaa.edu.cn>
2026-03-02 14:43:38 +0800

    Fix float calculation error due to FPCCR config

--- target/pixhawk/fmu-v6c/board/system_stm32h7xx.c
+++ target/pixhawk/fmu-v6c/board/system_stm32h7xx.c
@@ -186,6 +186,10 @@ void SystemInit (void)
   /* FPU settings ------------------------------------------------------------*/
   #if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
     SCB->CPACR |= ((3UL << (10*2))|(3UL << (11*2)));  /* set CP10 and CP11 Full Access */
+    /* Ensure FPCCR ASPEN and LSPEN are set at startup
+       This sets bits 31 and 30 (0xC0000000) in FPCCR. */
+    FPU->FPCCR |= (FPU_FPCCR_ASPEN_Msk | FPU_FPCCR_LSPEN_Msk);
+
   #endif
   /* Reset the RCC clock configuration to the default reset state ------------*/
 
