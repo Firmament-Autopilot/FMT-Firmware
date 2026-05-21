97987651ea72da8c1b49bfe3ea8482c2457410e9
Chenliang Pan <panchenliang@nuaa.edu.cn>
2026-05-21 10:26:30 +0800

    update logging method when mlog_mode==2

--- src/module/param/sys_param.c
+++ src/module/param/sys_param.c
@@ -25,13 +25,13 @@ static param_t __param_list[] = {
     /* Mavlink component ID */
     PARAM_UINT8(MAV_COMP_ID, 1, false),
     /* Determines when to start and stop logging.
-	0: disabled
-	1: when armed until disarm
-	2: from boot until disarm
-	3: from boot until shutdown  */
+        0: disabled
+        1: when armed until disarm
+        2: from boot until disarm, next arm will restart logging
+        3: from boot until shutdown  */
     PARAM_INT32(MLOG_MODE, 1, false),
-    /* Endless mission mode, when mission complete, 
-	it will start from the first mission data again */
+    /* Endless mission mode, when mission complete,
+        it will start from the first mission data again */
     PARAM_UINT8(LOOP_MISSION, 0, false),
     /* Send obc heartbeat signal to gcs */
     PARAM_UINT8(OBC_HEARTBEAT, 0, false),
